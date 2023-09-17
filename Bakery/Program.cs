e260586dad11fb2059f98ba20ff6965618333331
Greg <Jariye05@gmail.com>
2023-09-16 23:34:29 -0700

    trying to add program.cs

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,17 +1,24 @@
 using System;
 using Bakery.Models;
 
+
 namespace Bakery
 {
   class Program
   {
     static void Main()
     {
-      // Console.Writeline("Welcome to Greg's Bakery!");
-      // Console.Writeline("How many loaves of bread would you like to purchase?");
-      // string breadInput = Console.ReadLine();
-      // int breadInputInt = int.Parse(breadInput);
-      // Bread order = new Bread(breadInputInt);
+       Console.WriteLine("Welcome to Greg's Bakery!");
+       Console.WriteLine("How many loaves of bread/pastry would you like to purchase?");
+       Console.WriteLine("Bread cost $5 a loaf and for every two loaf, you get one for free!");
+       string breadInput = Console.ReadLine();
+       int breadInputInt = int.Parse(breadInput);
+       int PastryInputInt = int.parse(PastryInputInt);
+       Bread order = new Bread(breadInputInt); 
+       Pastry order = new Pastry(PastryInputInt);
+       Pastry.GetPasteryOrderCost();
+       Bread.GetBreadOrderCost();
+      Console.WriteLine($"Your total will be: ${Bread.TotalCost + Pastry.TotalCost}");
     }
   }
 }
