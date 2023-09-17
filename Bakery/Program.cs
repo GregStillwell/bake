b609b171614094b6b760a406e1c5de980d5b8a88
Greg <Jariye05@gmail.com>
2023-09-16 23:59:32 -0700

    added program.cs

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -12,13 +12,15 @@ namespace Bakery
        Console.WriteLine("How many loaves of bread/pastry would you like to purchase?");
        Console.WriteLine("Bread cost $5 a loaf and for every two loaf, you get one for free!");
        string breadInput = Console.ReadLine();
-       int breadInputInt = int.Parse(breadInput);
-       int PastryInputInt = int.parse(PastryInputInt);
-       Bread order = new Bread(breadInputInt); 
-       Pastry order = new Pastry(PastryInputInt);
-       Pastry.GetPasteryOrderCost();
-       Bread.GetBreadOrderCost();
-      Console.WriteLine($"Your total will be: ${Bread.TotalCost + Pastry.TotalCost}");
+       Console.WriteLine("How many Patries would you like to buy?");
+       string pastryInput = Console.ReadLine();
+       int BreadQuantity = int.Parse(breadInput);
+       int PastryQuantity = int.Parse(pastryInput);
+       Bread bread = new Bread(BreadQuantity); 
+       Pastry pastry = new Pastry(PastryQuantity);
+       pastry.GetPasteryOrderCost();
+       bread.GetBreadOrderCost();
+      Console.WriteLine($"Your total will be: ${bread.TotalCost + pastry.TotalCost}");
     }
   }
 }
